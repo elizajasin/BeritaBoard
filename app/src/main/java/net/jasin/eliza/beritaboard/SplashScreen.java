8587a8d7ea1bdca937c0abed290537609d8efce3
elizajasin <eliza_jasin@hotmail.com>
2017-09-11 18:31:19 +0700

    add splashscreen

--- app/src/main/java/net/jasin/eliza/beritaboard/SplashScreen.java
+++ app/src/main/java/net/jasin/eliza/beritaboard/SplashScreen.java
@@ -1,13 +1,28 @@
 package net.jasin.eliza.beritaboard;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    TextView tittle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this,Home.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
